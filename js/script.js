cd2757ead8b126411631cd7ebad215ff5346d133
Aya ELsayed <ayaelsaid653@gmail.com>
2024-12-04 02:03:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
 function toggleMenu() {
-  const menu = document.querySelector('nav ul');
-  console.log('Toggling menu'); 
-  menu.classList.toggle('open');
+  const navMenu = document.querySelector("nav ul");
+  navMenu.classList.toggle("open"); 
 }
-
-const dropdownItems = document.querySelectorAll('li.dropdown > a');
-dropdownItems.forEach((item) => {
-  item.addEventListener('click', (event) => {
-    event.preventDefault();
-    const dropdownMenu = item.nextElementSibling;
-    console.log('Dropdown clicked', dropdownMenu); 
-    if (dropdownMenu) {
-      dropdownMenu.classList.toggle('open');
-    }
-  });
-});     
