fbf7e99ec063e8a55f436165c4cd721565e0d30f
Aya ELsayed <ayaelsaid653@gmail.com>
2024-12-14 19:42:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 function toggleMenu() {
   const navMenu = document.querySelector("nav ul");
-  navMenu.classList.toggle("open"); 
+  navMenu.classList.toggle("open");
 }
+const searchIcon = document.getElementById("search");
+const searchBar = document.querySelector(".search-bar");
+
+searchIcon.addEventListener("click", () => {
+  searchBar.classList.toggle("active");
+});
+// .................... privacy .............................
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+}); // rigth click XXXXXXX
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey && (e.key === "u" || e.key === "U")) ||
+    (e.ctrlKey && e.shiftKey && e.key === "I")
+  ) {
+    e.preventDefault();
+  }
+}); // (Ctrl+Shift+I / F12)   XXXXXXXXXXXXXXXXXXXXXxxx
+(function () {
+  const devtools = new Image();
+  Object.defineProperty(devtools, "id", {
+    get: function () {
+      alert("Warning: This could potentially harm your device!");
+    },
+  });
+  setInterval(function () {
+    console.log(devtools);
+  }, 1000);
+})();
